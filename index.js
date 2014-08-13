de84daef9ab044d97b6cf00d2dfe72b28be22979
jonschlinkert <dev@sellside.com>
2014-08-13 05:55:27 -0400

    only return an object, run

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * fenced-code-blocks <https://github.com/jonschlinkert/fenced-code-blocks>
+ * gfm-code-blocks <https://github.com/jonschlinkert/gfm-code-blocks>
  *
  * Copyright (c) 2014 Jon Schlinkert, contributors.
  * Licensed under the MIT License
@@ -7,60 +7,16 @@
 
 'use strict';
 
-var Cache = require('./hash');
-// var parser = module.exports =  new Cache();
+var re = require('gfm-code-block-regex')();
 
-var parser = module.exports;
+module.exports = function(str) {
+  var arr = [];
 
-parser.items = {};
-
-parser.set = function(key, value) {
-  parser.items[this.key(key)] = value;
-};
-
-parser.get = function(key, value) {
-  return parser.items[this.key(key)];
-};
-
-parser.key = function(key) {
-  return '__CODE_BLOCK_' + key + '__';
-};
-
-parser.writeCodeBlock = function (key) {
-  var block = this.items[key];
-  var code = '';
-  code += '```' + block.lang;
-  code += '\n' + block.code + '\n';
-  code += '```\n';
-  return '\n' + code + '\n';
-};
-
-parser.extract = function(str) {
-  var i = 0;
-  var fenced = /^\s*(`{3})\s*(\S+)?\s*([\s\S]+?)\s*(`{3})\s*(?:\n+|$)/gm;
-  var indented = /^( {4}[^\n]+\n*)+/gm;
-  console.log(str.match(indented));
-  if (fenced.exec(str)) {
-    str = str.replace(fenced, function (match, open, lang, code, close, i) {
-      parser.set(i, {lang: lang, code: code});
-      return match.replace(match, parser.key(i));
+  while(re.exec(str)) {
+    arr.push({
+      lang: RegExp.$2,
+      code: RegExp.$3
     });
   }
-  return str;
+  return arr;
 };
-
-parser.replace = function (str, obj) {
-  var heuristic = /(__CODE_BLOCK_([0-9]+)__)/g;
-  str = parser.extract(str);
-
-  return str.match(heuristic).reduce(function(content, a, b, c) {
-    return content.replace(a, this.writeCodeBlock(a));
-  }.bind(this), str);
-};
-
-
-
-var file = require('fs-utils');
-var readme = file.readFileSync('test/fixtures/assemble.md');
-console.log(parser.replace(readme))
-// console.log(replace(readme))
