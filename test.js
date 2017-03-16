a7c5f9893bd35546037f1de11ad2854c97a8a3d8
jonschlinkert <jon.schlinkert@sellside.com>
2017-03-16 11:50:16 -0400

    use assert, update tests

--- test.js
+++ test.js
@@ -7,47 +7,44 @@
 
 'use strict';
 
-/* deps:mocha */
-require('should');
 require('mocha');
-var helpers = require('test-helpers')({dir: './'});
-var readme = helpers.readFixture('README.md');
+var assert = require('assert');
 var codeBlocks = require('./');
 
 describe('code block', function () {
   it('should return the code blocks from the fixture.', function () {
-    var blocks = codeBlocks(readme);
-    blocks.should.be.an.object;
-  });
-
-  it('should get the language from a code block.', function () {
-    var readme = helpers.readFixture('README.md');
-    codeBlocks(readme)[0].lang.should.equal('bash');
+    var blocks = codeBlocks('text\n```js\nfoo\n```\ntext');
+    assert(blocks);
+    assert.equal(typeof blocks, 'object');
   });
 
   it('should get a code block from a string.', function () {
-    var blocks = codeBlocks('```js\nfoo\n```');
-    blocks[0].block.should.equal('```js\nfoo\n```');
-    blocks[0].lang.should.equal('js');
-    blocks[0].code.should.equal('foo');
-    blocks[0].start.should.equal(0);
-    blocks[0].end.should.equal(13);
+    var blocks = codeBlocks('text\n```js\nfoo\n```\ntext');
+    assert.equal(blocks[0].block, '```js\nfoo\n```');
+    assert.equal(blocks[0].lang, 'js');
+    assert.equal(blocks[0].code, '\nfoo\n');
+    assert.equal(blocks[0].start, 5);
+    assert.equal(blocks[0].end, 18);
+  });
+
+  it('should get the language from a code block.', function () {
+    assert.equal(codeBlocks('text\n```js\nfoo\n```\ntext')[0].lang, 'js');
   });
 
   it('should get a code block from a string arounded with other data (text).', function () {
     var blocks = codeBlocks('abc\n```bash\nbar\n```\nxyz');
-    blocks[0].block.should.equal('```bash\nbar\n```');
-    blocks[0].lang.should.equal('bash');
-    blocks[0].code.should.equal('bar');
-    blocks[0].start.should.equal(4);
+    assert.equal(blocks[0].block, '```bash\nbar\n```');
+    assert.equal(blocks[0].lang, 'bash');
+    assert.equal(blocks[0].code, '\nbar\n');
+    assert.equal(blocks[0].start, 4);
   });
 
   it('should have position properties `start` and `end`.', function () {
     var blocks = codeBlocks('usage\n```js\nvar qux = 123;\n```\nxyz');
-    blocks[0].block.should.equal('```js\nvar qux = 123;\n```');
-    blocks[0].lang.should.equal('js');
-    blocks[0].code.should.equal('var qux = 123;');
-    blocks[0].start.should.equal(6);
-    blocks[0].end.should.equal(30);
+    assert.equal(blocks[0].block, '```js\nvar qux = 123;\n```');
+    assert.equal(blocks[0].lang, 'js');
+    assert.equal(blocks[0].code, '\nvar qux = 123;\n');
+    assert.equal(blocks[0].start, 6);
+    assert.equal(blocks[0].end, 30);
   });
 });
